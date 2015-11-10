b73726a515ec3a6354286b3494e3bff1b9467e61
Philipp Schirmer <philipp@schirmer-online.de>
2015-11-10 12:24:25 +0100

    tests are cool

---
+++ src/test/de/hpi/ir/yahoogle/test/AllTests.java
@@ -0,0 +1,11 @@
+package de.hpi.ir.yahoogle.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EliasDeltaReadWriteTest.class, ByteReadWriteTest.class })
+public class AllTests {
+
+}
