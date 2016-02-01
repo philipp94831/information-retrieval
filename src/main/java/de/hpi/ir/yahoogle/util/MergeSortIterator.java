0b0ef30478f855de6181205e2ca505a06416679f
Philipp Schirmer <philipp@schirmer-online.de>
2016-02-01 19:15:16 +0100

    fix

--- src/main/java/de/hpi/ir/yahoogle/util/MergeSortIterator.java
+++ src/main/java/de/hpi/ir/yahoogle/util/MergeSortIterator.java
@@ -8,13 +8,15 @@ import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-public class MergeSortIterator<S extends Iterable<V>, V extends Mergeable<K>, K extends Comparable<K>> implements Iterator<List<V>> {
+public class MergeSortIterator<S extends Iterable<V>, V extends Mergeable<K>, K extends Comparable<K>>
+		implements Iterator<List<V>> {
 
 	private TreeMap<K, List<MergeTreeEntry<V>>> candidates = new TreeMap<>();
 	private List<Iterator<V>> iterators;
 
 	public MergeSortIterator(List<S> sources) {
-		iterators = sources.stream().map(S::iterator).collect(Collectors.toList());
+		iterators = sources.stream().map(S::iterator)
+				.collect(Collectors.toList());
 		for (int i = 0; i < iterators.size(); i++) {
 			Iterator<V> iterator = iterators.get(i);
 			if (iterator.hasNext()) {
@@ -32,18 +34,23 @@ public class MergeSortIterator<S extends Iterable<V>, V extends Mergeable<K>, K 
 	public List<V> next() {
 		Entry<K, List<MergeTreeEntry<V>>> next = candidates.pollFirstEntry();
 		List<V> values = new ArrayList<>();
-		for(MergeTreeEntry<V> entry : next.getValue()) {
+		for (MergeTreeEntry<V> entry : next.getValue()) {
 			values.add(entry.getValue());
 			Iterator<V> iterator = iterators.get(entry.getIndex());
 			if (iterator.hasNext()) {
 				put(iterator.next(), entry.getIndex());
-			}			
+			}
 		}
 		return values;
 	}
 
 	private void put(V value, Integer i) {
 		MergeTreeEntry<V> entry = new MergeTreeEntry<>(i, value);
-		candidates.merge(value.getKey(), Arrays.asList(entry), (v1, v2) -> {v1.addAll(v2); return v1;});
+		candidates.merge(value.getKey(), Arrays.asList(entry), (v1, v2) -> {
+			ArrayList<MergeTreeEntry<V>> list = new ArrayList<>();
+			list.addAll(v1);
+			list.addAll(v2);
+			return list;
+		});
 	}
 }
